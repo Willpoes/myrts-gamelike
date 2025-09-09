f882b7e03cb7ef7d830b5c92f0012d64153effde
WillPoes <wilerutap@gmail.com>
2025-09-09 16:13:38 -0500

    aplicado la base de rotacion, panning, zooming y padding dentro de la camara

--- Assets/Scripts/Player/CameraConfig.cs
+++ Assets/Scripts/Player/CameraConfig.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 
-namespace GameDevTV.RTS.Player
+namespace ideadores.RTS.Player
 {
-    [System.Serializable]
+    [System.Serializable] //serialziable class que se quiere mirar en el inspector
     public class CameraConfig
     {
+
+        #region panning, rotation, zooming: slerp y funciones setting for the reset VARIABLES
+        [field: SerializeField] public float KeyboardPanSpeed { get; private set; } = 5;
+        [field: SerializeField] public float ZoomSpeed { get; private set; } = 1;
+        [field: SerializeField] public float RotationSpeed { get; private set; } = 1;
+        [field: SerializeField] public float MinZoomDistance { get; private set; } = 7.5f;
+        
         [field: SerializeField] public bool EnableEdgePan { get; private set; } = true;
         [field: SerializeField] public float MousePanSpeed { get; private set; } = 5;
         [field: SerializeField] public float EdgePanSize { get; private set; } = 50;
+        #endregion
 
-        [field: SerializeField] public float KeyboardPanSpeed { get; private set; } = 5;
 
-        [field: SerializeField] public float ZoomSpeed { get; private set; } = 1;
-        [field: SerializeField] public float MinZoomDistance { get; private set; } = 7.5f;
 
-        [field: SerializeField] public float RotationSpeed { get; private set; } = 1;
+
     }
 }
